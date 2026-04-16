c45bd80b95e1302f2089c7ae76d5d345823ca64b
Pratik Kothare <kotharepratik129@gmail.com>
2026-04-16 23:03:39 +0530

    Implement Joi schema validation with middleware for listing data

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 const MONGO_URL = process.env.MONGO_URL;
 main()
@@ -31,6 +32,17 @@ app.get("/", (req, res) => {
   res.send("Hi Am Pratik");
 });
 
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body); // for all defining schema condition match or not 
+    console.log(error);
+    if(error){
+      let errMsg = error.details.map((el)=>el.message).join(",");
+      throw new ExpressError(404,errMsg);
+    }else{
+      next();
+    }
+}
+
 //Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
   let allListings = await Listing.find({});
@@ -52,16 +64,14 @@ app.get("/listings/:id",wrapAsync( async (req, res) => {
 
 // Create Route
 app.post(
-  "/listings",
+  "/listings", validateListing, 
   wrapAsync(async (req, res, next) => {
     // let{title,description,location,cuntry,price} = req.body;
     //or
     // let listing = req.body.listing;
     // new Listing(listing);
     //or
-    if(!req.body.listing){
-      throw new ExpressError(500,"Send valid data for listing");
-    }
+    
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -77,17 +87,14 @@ app.get("/listings/:id/edit",wrapAsync( async (req, res) => {
 
 // Update Route
 
-app.put("/listings/:id",wrapAsync( async (req, res) => {
-  if(!req.body.listing){
-      throw new ExpressError(500,"Send valid data for listing");
-    }
+app.put("/listings/:id",validateListing, wrapAsync( async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
 }));
 //Delete Route
 
-app.delete("/listings/:id",wrapAsync( async (req, res) => {
+app.delete("/listings/:id", wrapAsync( async (req, res) => {
   let { id } = req.params;
   let deletingListing = await Listing.findByIdAndDelete(id);
   console.log(deletingListing);
@@ -116,7 +123,8 @@ app.all(/(.*)/, (req, res, next) => {
 
 app.use((err, req, res, next) => {
   let{statuscode=500,message="Somthing went wrong!"} = err;
-  res.status(statuscode).send(message);
+  // res.status(statuscode).send(message);
+   res.status(statuscode).render("listings/error.ejs",{message});
   // res.send("somthing went wrong");
 });
 
