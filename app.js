d5bab66f61ea0f44d6d29ec905b2ead493c70e9c
Pratik Kothare <kotharepratik129@gmail.com>
2026-05-26 22:53:28 +0530

    add delete review functionality with  operator and cascade delete for listing reviews

--- app.js
+++ app.js
@@ -142,10 +142,20 @@ app.post("/listings/:id/reviews",validateReview,wrapAsync( async (req, res) => {
   await newReview.save();
   await listing.save();
 
-
   res.redirect(`/listings/${listing._id}`);
 }));
 
+
+//Delete Review Route
+
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req,res)=>{
+  let {id,reviewId} = req.params;
+
+  await Listing.findByIdAndUpdate(id,{$pull: {reviews: reviewId}});
+  await Review.findByIdAndDelete(reviewId);
+
+  res.redirect(`/listings/${id}`);
+}))
 // app.get("/testListing",async(req,res)=>{
 //     let sampleListing = new Listing({
 //         title:"Orange City",
