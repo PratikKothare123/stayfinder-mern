dae2a1412e1b723e8d8a75f2fe5b55717107aa9b
PratikKothare123 <kotharepratik129@gmail.com>
2026-06-19 07:24:24 +0530

    implement MVC architecture for listings,review and users

---
+++ controllers/listings.js
@@ -0,0 +1,65 @@
+const Listing = require("../models/listing.js");
+module.exports.index = async (req, res) => {
+    let allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  }
+
+module.exports.renderNewForm = (req, res) => {
+  res.render("listings/new.ejs");
+};
+
+
+module.exports.createListing = async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    await newListing.save();
+    req.flash("success", "New listing created!");
+    return res.redirect("/listings");
+  }
+
+
+module.exports.showListing = async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id)
+      .populate({
+        path: "reviews",
+        populate: {
+          // for every review print author name ,So i create nesting populate
+          path: "author",
+        },
+      })
+      .populate("owner");
+    if (!listing) {
+      req.flash("error", "Listing you requested for does not exist!");
+      res.redirect("/listings");
+    }
+    res.render("listings/show.ejs", { listing });
+  };
+
+
+  module.exports.renderEditForm = async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+      req.flash("error", "Listing you requested for does not exist!");
+      return res.redirect("/listings");
+    }
+    res.render("listings/edit.ejs", { listing });
+  }
+
+
+  module.exports.updateListing = async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing updated!");
+    res.redirect(`/listings/${id}`);
+  }
+
+
+  module.exports.destroyListing = async (req, res) => {
+    let { id } = req.params;
+    let deletingListing = await Listing.findByIdAndDelete(id);
+    console.log(deletingListing);
+    req.flash("success", "Listing deleted !");
+    res.redirect("/listings");
+  }
---
+++ controllers/users.js
@@ -0,0 +1,44 @@
+const User = require("../models/user");
+
+module.exports.renderSignupForm = (req, res) => {
+  res.render("users/signup.ejs");
+}
+
+module.exports.signup = async (req, res) => {
+  try {
+    let { username, password, email } = req.body;
+    const newUser = await new User({ email, username });
+    const registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.login(registeredUser, (err)=>{
+      if(err){
+        return next(err);
+      }
+       req.flash("success", "Welcome to StayFinder");
+       res.redirect("/listings");
+    }); 
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+}
+
+module.exports.renderLoginForm = (req, res) => {
+  res.render("users/login.ejs");
+}
+
+module.exports.login = async(req, res) => {
+    req.flash("success","Wlc back to StayFinder");
+    let redirectUrl = res.locals.redirectUrl || "/listings"; // suppose we direct login occuer error Bcz, req dosent to to the isLoggedIn and saveRedirectUrl
+    res.redirect(redirectUrl);
+  }
+
+module.exports.logout = (req,res,next)=>{
+   req.logout((err)=>{
+    if(err){
+      return nexr();
+    }
+    req.flash("success","you are logged out!");
+    res.redirect("/listings");
+  });
+}
